c1f646e65ef385a7f36b9569ebc051fbaf3d1819
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-01 00:29:44 +0600

    setitem

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react"
-import { garlary } from "./gallary"
+import { useEffect, useState } from "react";
+import { garlary } from "./gallary";
 
 function App() {
-  const [data,setData]=useState([]);
-  const [collection,setCalletion]=useState([]);
- useEffect(()=>{
-  setData(garlary)
- },[])
-
- console.log(data);
+  const [data, setData] = useState([]);
+  const [collection, setCalletion] = useState([]);
+  useEffect(() => {
+    setData(garlary);
+    setCalletion([...new Set(garlary.map((item)=> item.title))]);
+    
+  }, []);
+  
+  console.log(collection);
   return (
     <>
-      
+      <div className="galeryrepper">
+        <div className="galarycontainer">
+          {data.map((item) => (
+            <div className="item">
+              <img src={item.img} key={item.id} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
