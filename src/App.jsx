bc9af0bd8fd6b71880ab220f93555ffb620ea897
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-01-31 23:46:30 +0600

    imge add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
+import { useEffect, useState } from "react"
+import { garlary } from "./gallary"
 
 function App() {
-  
+  const [data,setData]=useState([]);
+  const [collection,setCalletion]=useState([]);
+ useEffect(()=>{
+  setData(garlary)
+ },[])
 
+ console.log(data);
   return (
     <>
       
